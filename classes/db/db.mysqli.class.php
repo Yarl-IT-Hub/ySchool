7e94615461c479b591b5fc00b4da212e67402e88
Jason <jasond@hsenidmobile.com>
2012-01-24 23:33:26 +0800

    remving php files if needed can be taken from rev

--- classes/db/db.mysqli.class.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include("data.php");
-$mysqli = new mysqli($DatabaseServer, $DatabaseUsername, $DatabasePassword, $DatabaseName);
-if (mysqli_connect_errno()) {
-echo("Failed to connect, the error message is : ".
-mysqli_connect_error());
-exit();
-}
-?>
