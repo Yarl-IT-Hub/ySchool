d58a676ecf07d3c6904cbeff351bf619c31a0764
Jason <jasond@hsenidmobile.com>
2012-01-25 00:31:18 +0800

    commiting initial maven structure

---
+++ modules/reporting/src/main/java/org/yalithub/yschool/App.java
@@ -0,0 +1,13 @@
+package org.yalithub.yschool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
