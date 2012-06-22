dca0b736e499ed18d11c415e0406d7dbd9a0c920
gayathiry <gayathiry.pera@gmail.com>
2012-06-22 20:09:11 +0530

    Added select option fields

--- yschool/src/main/java/org/yarlithub/yschool/student/StudentBean.java
+++ yschool/src/main/java/org/yarlithub/yschool/student/StudentBean.java
@@ -18,10 +18,12 @@ import org.yarlithub.yschool.repository.Student;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import org.hibernate.Transaction;
+import javax.faces.model.SelectItem;
+import org.yarlithub.yschool.repository.House;
 import org.yarlithub.yschool.repository.Status;
-import org.yarlithub.yschool.repository.util.HibernateUtil;
+
 
 /**
  * $LastChangedDate$
@@ -37,13 +39,15 @@ public class StudentBean implements Serializable {
     public Student student = new Student();
     private Student selectedStudent;
     private List<Student> studentList;
-    
+    private List<SelectItem> genderList;
+    private List<SelectItem> houseList;
+    private List<SelectItem> studentStatusList;
+    private List<SelectItem> calenderYearList;
     
 
     public StudentBean() {
         student = new Student();
-        selectedStudent = new Student();
-        
+        selectedStudent = new Student();        
     }
 
     public void submit() {
@@ -89,5 +93,78 @@ public class StudentBean implements Serializable {
      */
     public void setStudentList(List<Student> studentList) {
         this.studentList = studentList;
+    }
+
+    /**
+     * @return the genderList
+     */
+    public List<SelectItem> getGenderList() {
+        if(genderList == null){
+           genderList = new ArrayList<>();
+           genderList.add(new SelectItem("MALE"));
+           genderList.add(new SelectItem("FEMALE"));
+        }        
+        return genderList;
+    }
+
+    /**
+     * @param genderList the genderList to set
+     */
+    public void setGenderList(List<SelectItem> genderList) {
+        this.genderList = genderList;
+    }
+
+    /**
+     * @return the houseList
+     */
+    public List<SelectItem> getHouseList() {
+        if(houseList == null){
+           houseList = new ArrayList<>();
+           houseList.add(new SelectItem(House.BLUE));
+           houseList.add(new SelectItem(House.GREEN));
+           houseList.add(new SelectItem(House.RED));
+        }        
+        return houseList;
+    }
+
+    /**
+     * @param houseList the houseList to set
+     */
+    public void setHouseList(List<SelectItem> houseList) {
+        this.houseList = houseList;
+    }
+
+    /**
+     * @return the studentStatusList
+     */
+    public List<SelectItem> getStudentStatusList() {
+        if(studentStatusList == null){
+           studentStatusList = new ArrayList<>();
+           studentStatusList.add(new SelectItem(Status.COMPLETED));
+           studentStatusList.add(new SelectItem(Status.PENDING));
+           studentStatusList.add(new SelectItem(Status.POSTPONED));
+        }       
+        return studentStatusList;
+    }
+
+    /**
+     * @param studentStatusList the studentStatusList to set
+     */
+    public void setStudentStatusList(List<SelectItem> studentStatusList) {
+        this.studentStatusList = studentStatusList;
+    }
+
+    /**
+     * @return the calenderYearList
+     */
+    public List<SelectItem> getCalenderYearList() {
+        return calenderYearList;
+    }
+
+    /**
+     * @param calenderYearList the calenderYearList to set
+     */
+    public void setCalenderYearList(List<SelectItem> calenderYearList) {
+        this.calenderYearList = calenderYearList;
     }
 }
