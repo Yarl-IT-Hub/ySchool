bc6e4c273e82502812efd55364943875b484c3c9
Jason <jasond@hsenidmobile.com>
2012-07-26 04:37:44 +0800

    1) ManagedBean injection added 2) log4j location changed.
    todo
    -----
    need to make jetty:run work

--- yschool/src/main/java/org/yarlithub/yschool/student/StudentBean.java
+++ yschool/src/main/java/org/yarlithub/yschool/student/StudentBean.java
@@ -12,22 +12,22 @@
  */
 package org.yarlithub.yschool.student;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.apache.log4j.Logger;
+import org.yarlithub.yschool.repository.House;
+import org.yarlithub.yschool.repository.Status;
+import org.yarlithub.yschool.repository.Student;
+import org.yarlithub.yschool.util.InitialDateLoaderUtil;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
-import org.apache.log4j.Logger;
-import org.yarlithub.yschool.repository.House;
-import org.yarlithub.yschool.service.InitialDateLoaderService;
-import org.yarlithub.yschool.repository.Status;
-import org.yarlithub.yschool.repository.Student;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -41,15 +41,15 @@ public class StudentBean implements Serializable {
 
     private static final Logger logger = Logger.getLogger(StudentBean.class);
 
-//    @ManagedProperty(value = "#{initialDateLoaderService}")
-    private transient InitialDateLoaderService initialDateLoaderService;
+    @ManagedProperty(value = "#{initialDateLoaderUtil}")
+    private InitialDateLoaderUtil initialDateLoaderUtil;
 
     public Student student;
     private List<Student> studentList;
-    private List<SelectItem> genderList = Arrays.asList(new SelectItem("MALE"), new SelectItem("FEMALE"));
-    private List<SelectItem> houseList = Arrays.asList();
-    private List<SelectItem> studentStatusList = Arrays.asList(new SelectItem(Status.COMPLETED), new SelectItem(Status.PENDING), new SelectItem(Status.POSTPONED));
-    private List<SelectItem> calenderYearList,gradeList,divisionList,mediumList,subjectList;
+    private List<String> genderList = Arrays.asList("Male", "Female");
+    private List<String> houseList;
+    private List<Status> studentStatusList = Arrays.asList(Status.COMPLETED, Status.PENDING, Status.POSTPONED);
+    private List<String> calenderYearList,gradeList,divisionList,mediumList,subjectList;
 
     public StudentBean() {
         logger.info("initiation a new student bean");
@@ -58,7 +58,7 @@ public class StudentBean implements Serializable {
 
     @PostConstruct
     public void init() {
-        initialDateLoaderService = new InitialDateLoaderService();
+        logger.info("loading initial data [" + initialDateLoaderUtil + "]");
         loadInitData();
     }
 
@@ -101,26 +101,26 @@ public class StudentBean implements Serializable {
     /**
      * @return the genderList
      */
-    public List<SelectItem> getGenderList() {
+    public List<String> getGenderList() {
         return genderList;
     }
 
     /**
      * @param genderList the genderList to set
      */
-    public void setGenderList(List<SelectItem> genderList) {
+    public void setGenderList(List<String> genderList) {
         this.genderList = genderList;
     }
 
     /**
      * @return the houseList
      */
-    public List<SelectItem> getHouseList() {
+    public List<String> getHouseList() {
         if(houseList == null){
            houseList = new ArrayList<>();
-           houseList.add(new SelectItem(House.RED));
-           houseList.add(new SelectItem(House.YELLOW));
-           houseList.add(new SelectItem(House.BLUE));
+           houseList.add(House.RED.name());
+           houseList.add(House.YELLOW.name());
+           houseList.add(House.BLUE.name());
         }
         return houseList;
     }
@@ -128,33 +128,33 @@ public class StudentBean implements Serializable {
     /**
      * @param houseList the houseList to set
      */
-    public void setHouseList(List<SelectItem> houseList) {
+    public void setHouseList(List<String> houseList) {
         this.houseList = houseList;
     }
 
     /**
      * @return the studentStatusList
      */
-    public List<SelectItem> getStudentStatusList() {
+    public List<Status> getStudentStatusList() {
         return studentStatusList;
     }
 
     /**
      * @param studentStatusList the studentStatusList to set
      */
-    public void setStudentStatusList(List<SelectItem> studentStatusList) {
+    public void setStudentStatusList(List<Status> studentStatusList) {
         this.studentStatusList = studentStatusList;
     }
 
     /**
      * @return the calenderYearList
      */
-    public List<SelectItem> getCalenderYearList() {
+    public List<String> getCalenderYearList() {
         if(calenderYearList == null){
            calenderYearList = new ArrayList<>();
-           calenderYearList.add(new SelectItem(2012));
-           calenderYearList.add(new SelectItem(2011));
-           calenderYearList.add(new SelectItem(2010));
+           calenderYearList.add("2012");
+           calenderYearList.add("2011");
+           calenderYearList.add("2010");
         }       
         
         return calenderYearList;
@@ -163,76 +163,75 @@ public class StudentBean implements Serializable {
     /**
      * @param calenderYearList the calenderYearList to set
      */
-    public void setCalenderYearList(List<SelectItem> calenderYearList) {
+    public void setCalenderYearList(List<String> calenderYearList) {
         this.calenderYearList = calenderYearList;
     } 
     
     public void loadInitData(){
-        initialDateLoaderService.setDataType("PRE_GRADE");
-        initialDateLoaderService.setDataList(Arrays.asList("1", "2", "3", "4", "5"));
-        setGradeList((List<SelectItem>) initialDateLoaderService.loadData("PRE_GRADE"));
-        setMediumList((List<SelectItem>) initialDateLoaderService.loadData("PRE_MEDIUM"));
-        setSubjectList((List<SelectItem>) initialDateLoaderService.loadData("PRE_SUBJECT"));
-        setDivisionList((List<SelectItem>) initialDateLoaderService.loadData("PRE_DIVISION"));
+        System.out.println(initialDateLoaderUtil);
+        setGradeList(initialDateLoaderUtil.loadData("PRE_GRADE"));
+        setMediumList(initialDateLoaderUtil.loadData("PRE_MEDIUM"));
+        setSubjectList(initialDateLoaderUtil.loadData("PRE_SUBJECT"));
+        setDivisionList(initialDateLoaderUtil.loadData("PRE_DIVISION"));
     }
 
     /**
      * @return the gradeList
      */
-    public List<SelectItem> getGradeList() {
+    public List<String> getGradeList() {
         return gradeList;
     }
 
     /**
      * @param gradeList the gradeList to set
      */
-    public void setGradeList(List<SelectItem> gradeList) {
+    public void setGradeList(List<String> gradeList) {
         this.gradeList = gradeList;
     }
 
     /**
      * @return the divisionList
      */
-    public List<SelectItem> getDivisionList() {
+    public List<String> getDivisionList() {
         return divisionList;
     }
 
     /**
      * @param divisionList the divisionList to set
      */
-    public void setDivisionList(List<SelectItem> divisionList) {
+    public void setDivisionList(List<String> divisionList) {
         this.divisionList = divisionList;
     }
 
     /**
      * @return the mediumList
      */
-    public List<SelectItem> getMediumList() {
+    public List<String> getMediumList() {
         return mediumList;
     }
 
     /**
      * @param mediumList the mediumList to set
      */
-    public void setMediumList(List<SelectItem> mediumList) {
+    public void setMediumList(List<String> mediumList) {
         this.mediumList = mediumList;
     }
 
     /**
      * @return the subjectList
      */
-    public List<SelectItem> getSubjectList() {
+    public List<String> getSubjectList() {
         return subjectList;
     }
 
     /**
      * @param subjectList the subjectList to set
      */
-    public void setSubjectList(List<SelectItem> subjectList) {
+    public void setSubjectList(List<String> subjectList) {
         this.subjectList = subjectList;
     }
 
-    public void setInitialDateLoaderService(InitialDateLoaderService initialDateLoaderService) {
-        this.initialDateLoaderService = initialDateLoaderService;
+    public void setInitialDateLoaderUtil(InitialDateLoaderUtil initialDateLoaderUtil) {
+        this.initialDateLoaderUtil = initialDateLoaderUtil;
     }
 }
