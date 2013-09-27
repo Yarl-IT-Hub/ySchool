95a24021697010589bb53b54ab9e53b4a342bcbc
Jay-Krish <raajjya@gmail.com>
2013-09-27 20:15:11 +0530

    initiating exam module; corrections addnew staff GUI

---
+++ yschool/src/main/java/org/yarlithub/yschool/web/examination/ExaminationBean.java
@@ -0,0 +1,21 @@
+package org.yarlithub.yschool.web.examination;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+
+/**
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+
+@ManagedBean
+@Scope(value = "session")
+@Controller
+public class ExaminationBean implements Serializable {
+
+}
