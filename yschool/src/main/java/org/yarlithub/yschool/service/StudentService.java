cdfd4f0301ad873ac60d7b2480042850a28e0776
amaaniy <amaaniy@gmail.com>
2013-10-23 22:30:39 +0530

    changes in staff files

--- yschool/src/main/java/org/yarlithub/yschool/service/StudentService.java
+++ yschool/src/main/java/org/yarlithub/yschool/service/StudentService.java
@@ -3,6 +3,10 @@ package org.yarlithub.yschool.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yarlithub.yschool.student.core.NewStudent;
+
+import java.util.Date;
 
 /**
  * TODO description
@@ -11,5 +15,11 @@ import org.springframework.stereotype.Service;
 public class StudentService {
     private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
+    @Transactional
+    public boolean addStudent(String addmision_No, String name, String fullname, String name_wt_initial, Date dob, String gender, String address) {
+        NewStudent newStudent= new NewStudent();
+        boolean success = newStudent.addNewStudent(addmision_No, name, fullname, name_wt_initial, dob, gender, address);
+        return success;
+    }
 
 }
