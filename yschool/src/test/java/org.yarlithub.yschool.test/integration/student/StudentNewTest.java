9c3b323ed7edaaf314926d218ffe4989d7c2db07
Jay-Krish <raajjya@gmail.com>
2014-01-21 03:56:22 +0530

    initiating integration testings

---
+++ yschool/src/test/java/org.yarlithub.yschool.test/integration/student/StudentNewTest.java
@@ -0,0 +1,31 @@
+package org.yarlithub.yschool.test.integration.student;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.yarlithub.yschool.web.student.StudentBean;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * These tests connects with database ySchool-1.0.5  and transfer data.
+ * Before Running tests make sure to import schema and initial data into the database.
+ */
+
+@ContextConfiguration(locations = {"/applicationContext.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+public class StudentNewTest {
+
+    @Test
+    @Transactional
+    public void insertStaffTest() {
+        StudentBean studentBean=new StudentBean();
+        studentBean.setName("wow");
+        System.out.println(studentBean.getName());
+
+    }
+}
