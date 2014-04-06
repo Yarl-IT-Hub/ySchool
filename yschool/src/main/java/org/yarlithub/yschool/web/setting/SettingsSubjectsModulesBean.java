47435af261565c066eebea10e67b73a3f2f3de75
Jay-Krish <raajjya@gmail.com>
2014-04-06 08:32:38 +0530

    settings subjects and modules feature

---
+++ yschool/src/main/java/org/yarlithub/yschool/web/setting/SettingsSubjectsModulesBean.java
@@ -0,0 +1,17 @@
+package org.yarlithub.yschool.web.setting;
+
+/**
+ * Created by jaykrish on 4/5/14.
+ */
+public class SettingsSubjectsModulesBean {
+    String pageNewSubject = "";
+    String pageNewModule = "";
+
+    public void setPageNewSubject() {
+        this.pageNewSubject = "_newSubject";
+    }
+
+    public void setPageNewModule() {
+        this.pageNewModule = pageNewModule;
+    }
+}
